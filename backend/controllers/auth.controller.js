516df755937ca082342d83febcf7ed5e25204b4c
Rohan <rohanj0306@gmail.com>
2024-04-23 22:39:06 +0530

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "signup",
+  });
+};
+export const login = async (req, res) => {
+  res.json({
+    data: "login",
+  });
+};
+export const logout = async (req, res) => {
+  res.json({
+    data: "logout",
+  });
+};
